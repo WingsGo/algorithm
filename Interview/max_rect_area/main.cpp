c52e8097c9aa6759f4755710b83aabfbf2a03ce7
WingC <1018957763@qq.com>
2018-04-18 17:34:16 +0800

    addd fin max rect area in histogram

--- Interview/max_rect_area/main.cpp
+++ Interview/max_rect_area/main.cpp
@@ -1,9 +1,30 @@
-#include <iostream>
+﻿#include <iostream>
+#include <vector>
 
 using namespace std;
 
 int main(int argc, char *argv[])
 {
-    cout << "Hello World!" << endl;
+    int n;
+    while (cin >> n) {
+        vector<int> heights(n, 0);
+        for (int i=0; i<n; ++i) {
+            int tmp;
+            cin >> tmp;
+            heights[i] = tmp;
+        }
+
+        int max_area = 0;
+        for (int i=0; i<n; ++i) {
+            int min_height = heights[i];
+            int current_area = min_height;
+            for (int j=i; j<n; ++j) {
+                min_height = min_height<heights[j] ? min_height : heights[j];
+                current_area = min_height * (j - i + 1);
+                max_area = max_area>current_area ? max_area : current_area;
+            }
+        }
+        cout << max_area << endl;
+    }
     return 0;
 }
